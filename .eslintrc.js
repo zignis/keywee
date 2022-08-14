dad4c26786d2ce50db19e184119cd396484992b7
HexM7 <velociraptorm77@gmail.com>
2022-08-14 19:25:12 +0530

    Fix rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ var options = {
   root: true,
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'no-restricted-syntax': [
       'error',
