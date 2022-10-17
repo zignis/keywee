80d1a29963517aeb3e126da014a98a51bae8521a
HexM7 <velociraptorm77@gmail.com>
2022-10-18 01:01:49 +0530

    Rename vectors

--- src/math/types/vector/index.ts
+++ src/math/types/vector/index.ts
@@ -1,4 +1,4 @@
-export type TwoDVector = [number, number];
-export type ThreeDVector = [number, number, number];
+export type Vector2D = [number, number];
+export type Vector3D = [number, number, number];
 
-export type Vector = TwoDVector | ThreeDVector;
+export type Vector = Vector2D | Vector3D;
